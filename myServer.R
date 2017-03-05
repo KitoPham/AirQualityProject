200631aa140f91f7fa6d0d909479bf80cac97ee1
Kito Pham <kpham97@live.com>
2017-03-04 18:44:20 -0800

    added notes

--- myServer.R
+++ myServer.R
@@ -2,10 +2,19 @@ library(dplyr)
 library(ggplot)
 library(shiny)
 
-
-
+rootUrl <- "https://api.openaq.org/v1/"
+### response <- GET(url)
+### body.data <- fromJSON(content(response,"text"))
+### body.data <- flatten(body.data)
 
 my.server<-function(input,output){
+  #https://api.openaq.org/v1/measurements?parameter= (ui.dropdown input, ex= pm25)
+  #append url with & date_to of slider input (use latest)
   
+  
+  #filter ggplot by country for world 
+  
+  
+  #onclick change to country clicked
   
 }
