4770d3823ea8150d2b4325d359e2515de46fa942
Kito Pham <kpham97@live.com>
2017-03-05 22:37:05 -0800

    first test commit

--- myServer.R
+++ myServer.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(ggplot)
 library(shiny)
+library(httr)
+library(jsonlite)
 
 rootUrl <- "https://api.openaq.org/v1/"
 ### response <- GET(url)
@@ -9,11 +11,27 @@ rootUrl <- "https://api.openaq.org/v1/"
 
 my.server<-function(input,output){
   #https://api.openaq.org/v1/measurements?parameter= (ui.dropdown input, ex= pm25)
+  urlHeader <- "https://api.openaq.org/v1/measurements"
+  urlParameter<-urlHeader + "?parameter="
   #append url with & date_to of slider input (use latest)
+  dateInput <- reactive{
+    return (input$date)
+  }
+  dataUrl <- urlParameter + dateInput()
+  airData <- GET(dataUrl)
+  airData <- content(airData,"text")
+  airData<- fromJSON(airData)
+  airData<- flatten(airData$results)
   
   
-  #filter ggplot by country for world 
-  
+  #filter ggplot by country for world
+  output$plot <- renderPlot(
+    map.frame = map_data("world")
+    plot <- ggplot(data = map.frame, na.rm = TRUE) +
+      geom_polygon(aes(x = long, y = lat, group = group, fill = GDP)) +
+      scale_fill_brewer(palette = "YlOrRd") +
+      coord_quickmap()
+  )
   
   #onclick change to country clicked
   
--- myUi.R
+++ myUi.R
@@ -1,3 +1,12 @@
+library(shiny)
 my.ui <- fluidpage(
-  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("parameter", "Select a gas", c("pm25", "pm10","so2", "o3","co","bc"))
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+  ),
+  sliderInput("date", timeFormat = "%F")
 )
