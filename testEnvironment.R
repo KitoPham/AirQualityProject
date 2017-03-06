88c4b5dd0438c7147f6543ad59e0065d9b898039
Kito Pham <kpham97@live.com>
2017-03-05 23:54:15 -0800

    set up map

--- testEnvironment.R
+++ testEnvironment.R
@@ -1,10 +1,33 @@
+library(maps)
+install.packages("rworldmap")
+library(rworldmap)
+library(maps)
+library(knitr)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
 #https://api.openaq.org/v1/measurements?parameter= (ui.dropdown input, ex= pm25)
 urlHeader <- "https://api.openaq.org/v1/measurements"
 urlParameter<-urlHeader + "?parameter="
 #append url with & date_to of slider input (use latest)
 
-dataUrl <- "https://api.openaq.org/v1/measurements?Parameter=pm25&Date_to=2012-12-12"
+dataUrl <- "https://api.openaq.org/v1/measurements?parameter=pm25&date_to=2015-12-06"
 airData <- GET(dataUrl)
 airData <- content(airData,"text")
 airData<- fromJSON(airData)
 airData<- flatten(airData$results)
+
+airData<- group_by(airData,country) %>% 
+            summarize(value = mean(value))
+
+
+
+map.frame <- map_data("world") %>% 
+  mutate(country = iso.alpha(region))
+airData <- left_join(map.frame,airData)
+
+ggplot(data = airData, na.rm = TRUE) +
+    geom_polygon(aes(x = long, y = lat, group = group, fill = value)) +
+    coord_quickmap()
+
+   
