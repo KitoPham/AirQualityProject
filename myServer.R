05a7133f3299f11124dd2d1a6e9082c9b82ef37c
Kito Pham <kpham97@live.com>
2017-03-09 03:35:55 -0800

    tabs and compare

--- myServer.R
+++ myServer.R
@@ -25,16 +25,28 @@ my.server<-function(input,output){
   
   mar.5.7 <- read.csv('data/Mar5-7.csv', stringsAsFactors=FALSE)
   
-  us.frame <- rbind(mar.5.7, mar.1.4, feb.27.28, feb.25.26,
+  us.all.frame <- rbind(mar.5.7, mar.1.4, feb.27.28, feb.25.26,
                       feb.21.24, feb.16.20, feb.11.15, feb.7.10)
   
-  us.frame <- aggregate(value~location + latitude + longitude + city, last.month, mean)
+  us.frame <- aggregate(value~location + latitude + longitude + city, us.all.frame, mean)
   
   us.frame <- filter(us.frame, latitude > 25) %>% 
     filter(latitude < 55)
   
-  us.frame$value = cut(us.frame$value, 10, labels = c('0 to 3', "3 to 6", "6 to 8", "8 to 10", "10 to 13",
-                                                      "13 to 16", "16 to 19", "19 to 22", "22 to 27", "NA"))
+  us.plot.frame <- us.frame
+  us.plot.frame$value = cut(us.frame$value, 10, labels = c('0 to 3', "3 to 6", "6 to 8", "8 to 10", "10 to 13","13 to 16", "16 to 19", "19 to 22", "22 to 27", "NA"))
+  
+  output$selectthis <- renderUI({
+    selectInput("city1", "City 1", unique(us.frame$city))
+  }) 
+  output$selectthat <- renderUI({
+    selectInput("city2", "City 2", unique(us.frame$city))
+  })
+  #updateSelectInput(session, "selectthis", choices = unique(us.frame$location))
+  #updateSelectInput(session, "selectthat", choices = unique(us.frame$location))
+    
+                                                                                                 
+  
 
 states <- map_data("state")
 ranges2 <- reactiveValues(x = NULL, y = NULL)
@@ -47,7 +59,7 @@ ranges2 <- reactiveValues(x = NULL, y = NULL)
     
   plot <- ggplot() + 
       geom_polygon(data = states, fill = "#629632", color = "#000000", aes(x = long, y = lat, group = group))  + 
-    geom_point(data = us.frame, aes(x = longitude, y = latitude, color = value)) +
+    geom_point(data = us.plot.frame, aes(x = longitude, y = latitude, color = value)) +
     scale_color_brewer(palette = 'Reds')
   
   return(plot)
@@ -60,8 +72,8 @@ ranges2 <- reactiveValues(x = NULL, y = NULL)
     
   plot <- ggplot() + 
       geom_polygon(data = states, fill = "#629632", color = "#000000", aes(x = long, y = lat, group = group))  + 
-    geom_point(data = us.frame, aes(x = longitude, y = latitude, color = value)) +
-    scale_color_brewer(palette = 'Reds') +
+      geom_point(data = us.plot.frame, aes(x = longitude, y = latitude, color = value, size = 10)) +
+      scale_color_brewer(palette = 'Reds') +
       coord_cartesian(xlim = ranges2$x, ylim = ranges2$y)
   
   return(plot)
@@ -84,37 +96,16 @@ ranges2 <- reactiveValues(x = NULL, y = NULL)
   selecttable <- reactiveValues(
     row = data.frame()
   )
-  #grabs selected points
-  observeEvent(input$point_click, {
-    selecttable$row <- nearPoints(us.frame, input$point_click, threshold = 10000, maxpoints = 1)
-  })
   
-  output$selected <- renderTable({
-    text <- "Click on a point for city"
-    if(!is.null(selecttable$row)){
-      newtable <- selecttable$row
-      if(nrow(selecttable$row) > 0){
-        text <- newtable[1,]
-      }
-    }
-    return(text)
-  })
-  
-  output$selectedLocation <- renderTable({
-    text <- "Click on a point for location"
-    if(!is.null(selecttable$row)){
-      newtable <- selecttable$row
-      if(nrow(selecttable$row) > 0){
-        text <- newtable[1,]
-      }
-    }
-    return(text)
-  })
-  
-  #output$overtime <- renderPlot({
- #   cityname <- selectedCity$city
-  #  cityFrame <- filter(us.frame, city == cityname)
-#  })
+  output$overtime <- renderPlot({
+    city1.frame <- filter(us.all.frame, city == input$city1);
+    city2.frame <- filter(us.all.frame, city == input$city2);
+    
+    plot <- ggplot() +
+      geom_point(data = city1.frame, mapping = aes(x = utc, y = value), color = "RED")+
+      geom_point(data = city2.frame, mapping = aes(x = utc, y = value), color = "BLUE")
+    return(plot)
+    })
   
   
   
--- myUi.R
+++ myUi.R
@@ -2,36 +2,35 @@ library(shiny)
 my.ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      tableOutput("selected"),
-      tableOutput("selectedLocation")
-      
-    ),
-    mainPanel(
-      plotOutput("plot", click = "point_click", 
-                 brush = brushOpts(
-                      id = "zoom_brush",
-                      resetOnNew = TRUE
-                      )
-      ),
-      
-      column(width = 8,
-             plotOutput("zoomed", click = "point_click2")
-             ),
-      
-      sliderInput("date","Date:",
+      uiOutput("selectthis"),
+      uiOutput("selectthat"),
+      sliderInput("date","Date:", 
                   min = as.Date("2017-02-07","%Y-%m-%d"),
                   max = as.Date("2017-03-07","%Y-%m-%d"),
                   value = c(as.Date("2016-03-01","%Y-%m-%d"),
                             as.Date("2017-03-07","%Y-%m-%d")),
-                  
-                  timeFormat = "%Y-%m-%d"),
-      plotOutput("overtime", hover = "overtimehover"),
+                  timeFormat = "%Y-%m-%d")
       
-      plotOutput("barCompare", hover = "barHover")
+    ),
+    mainPanel(
+      tabsetPanel(type="tabs",
+        tabPanel(
+          "Compare",
+          plotOutput("overtime", hover = "overtimehover")#,
+          #plotOutput("barCompare", hover = "barHover")
+        ),
+        
+        tabPanel(
+          "Map",
+          plotOutput("plot", 
+                     brush = brushOpts(id = "zoom_brush",resetOnNew = TRUE)
+                     ),
+          
+          plotOutput("zoomed")
+        )
+      )
     )
   )
-  
-  
-  
 )
+
 
